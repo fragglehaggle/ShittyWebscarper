b1d41971aaae575d986a0a68e58325d62e668055
fragglehaggle <mmkmmk3@gmail.com>
2021-03-03 23:25:36 -1000

    Update main recurse.py

--- main recurse.py
+++ main recurse.py
@@ -1,16 +1,17 @@
-  
 import requests
 import urllib.parse
 from bs4 import BeautifulSoup
+CMN_URL = 'https://2e.aonprd.com/' # url prefix for image linking - HTML doesn't store natively
 for n in range(7,1051):
     url = 'https://2e.aonprd.com/Monsters.aspx?ID={}'.format(n)
     page = requests.get(url)
+    #print(f"getting {url}")
     soup = BeautifulSoup(page.content, 'html.parser')  # I don't know currently, but I think it's just a HTML parse
     title = soup.find('h1', class_='title')  #Finds information based on the soup class parse the H1 class for the website - For AEONPRD the H1 text is the name of the monster
     IMG = soup.find_all('img', class_='thumbnail')  #Finds information from the soup parser, specifically the PNG file for monster thumbails
     if (title is not None):
         StripText = title.get_text()  # Get_text will parse the information retrieved from h1
-        print(StripText)
+        print(title)
     else:
         pass
     for im in IMG:
