a963b954d8d07e2595c17b70bbe131dfe9fd9246
fragglehaggle <mmkmmk3@gmail.com>
2021-03-03 23:14:47 -1000

    Add files via upload

---
+++ main recurse.py
@@ -0,0 +1,25 @@
+import requests
+import urllib.parse
+from bs4 import BeautifulSoup
+CMN_URL = 'https://2e.aonprd.com/' # url prefix for image linking - HTML doesn't store natively
+for n in range(7,1051):
+    url = 'https://2e.aonprd.com/Monsters.aspx?ID={}'.format(n)
+    page = requests.get(url)
+    #print(f"getting {url}")
+    soup = BeautifulSoup(
+    page.content, 'html.parser')  # I don't know currently, but I think it's just a HTML parse
+        #title = soup.title.children
+        #print(title)
+    title = soup.find(
+    'h1', class_='title')  #Finds information based on the soup class parse the H1 class for the website - For AEONPRD the H1 text is the name of the monster
+    IMG = soup.find_all('img', class_='thumbnail')  #Finds information from the soup parser, specifically the PNG file for monster thumbails
+    StripText = title.get_text()  # Get_text will parse the information retrieved from h1
+    if StripText is not None:
+        print(title)
+    else:
+        pass
+    for im in IMG:
+        (im['src'])
+        IMG_URL = urllib.parse.urljoin(CMN_URL, im['src'])#I have no idea what im is, but it collects the information after src
+        IMG_URL2 = IMG_URL.replace('\\', '/')#replaces backslashes with forwardslashes
+        print(IMG_URL2)
